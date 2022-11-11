712f8cf85d6d304cbd7de3913e622856283e97cf
Acacio-Silva <acaciosilva90@gmail.com>
2022-11-11 13:32:08 -0300

    finalizacao da tela de perguntas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 //setando o ejs como renderizador de html
 app.set('view engine', 'ejs');
 // especificando onde estão os arquivos estaticos do projeto.
@@ -11,6 +14,18 @@ app.use(express.static('public'));
 app.get("/", (req, res) =>{
     res.render("index")
 });
+
+app.get("/perguntar", (req, res) =>{
+    res.render("perguntar");
+})
+
+app.post("/salvarPergunta", (req, res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send("titulo");
+})
+
 
 app.listen(8080, ()=>{
     console.log("app rodando!")
